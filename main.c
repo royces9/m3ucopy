1740c0e88c81432f4d554721b4dd66f1aa161e0a
Royce <rrysato@gmail.com>
2017-07-16 16:58:10 -0700

    added some more

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-char *separateString(char input[]){
+char *separateString(char *input){
   char *tok, *input2 = (char *) malloc(strlen(input) * sizeof(char)), *token = "/";
   int i = 0, length = 0, tokenCount = 0;
 
@@ -30,12 +30,36 @@ char *separateString(char input[]){
     tok = strtok(NULL, token);
   }
   int end = strlen(separatedString);
+
   separatedString[end-1] = '\0';
+
   free(input2);
 
   return separatedString;
 }
 
+char *checkBeginning(char *input){
+  int a = 0, b = 0;
+
+  while(a*b == 0){
+    a = 1;
+    b = 1;
+    if(input[1] == '/' && input[0] == '/'){
+      *input++;
+      a = 0;
+    }
+
+    if(!strchr("/~", input[0])){
+      *input++;
+      b = 0;
+    }
+  }
+
+  char *output = (char *) malloc((strlen(input))*sizeof(char));
+
+  strcpy(output, input);
+  return output;
+}
 
 int main(int argc, char **argv){
   if(argc != 3){
@@ -44,50 +68,70 @@ int main(int argc, char **argv){
   }
 
   int sourceFileDesc, targetFileDesc, error = 0, n;  
-  char buffer[1024], *fileName, m3uLine[1024];
+  char buffer[1024], *fileName;//, m3uLine[1024], *temp;
+
+  char *m3uLine = (char *) malloc(1024*sizeof(char));
 
   char *m3u = (char *) malloc(strlen(argv[1]) * sizeof(char));
   strcpy(m3u, argv[1]);
 
   char *directory = (char *) malloc((strlen(argv[2])+1024) * sizeof(char));
-  strcpy(directory, argv[2]);
 
   FILE *m3uFile;
-
   m3uFile = fopen(m3u, "r");
 
-  while(fgets(m3uLine, sizeof(m3uLine), m3uFile)){
+  while(fgets(m3uLine, 1024, m3uFile)){
+
+    //Prepare string to be read
+    m3uLine = checkBeginning(m3uLine);
     fileName = separateString(m3uLine);
+
+    strcpy(directory, argv[2]);
+
+    for(int i = strlen(m3uLine)-1; m3uLine[i]; i--){
+      if(m3uLine[i] == '\n'){
+	m3uLine[i] = '\0';
+      }	
+    }
+    printf("Copying: %s\n", m3uLine);
+
+    if(directory[strlen(directory)] != '/'){
+      strcat(directory, "/");
+    }
     strcat(directory, fileName);
+
+    printf("Destination: %s\n\n", directory);
+    //
+
     
     sourceFileDesc = open(m3uLine, O_RDONLY);
-    targetFileDesc = open(directory, O_CREAT | O_WRONLY);
-    
-    error = read(sourceFileDesc, buffer, n);
+    targetFileDesc = open(directory, O_CREAT | O_WRONLY | O_APPEND, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXOTH);
+
+    error = read(sourceFileDesc, buffer, 10000000000);
     if(error == -1){
       printf("Error reading.\n");
       return 1;
     }
 
     n = error;
-
-    if(n == 0){
-      break;
-    }
+    printf("n = %d\n", n);
     
-    write(targetFileDesc, buffer, n);
+    error = write(targetFileDesc, buffer, n);
+
     if(error == -1){
       printf("Error writing.\n");
       return 1;
     }
+
+    close(sourceFileDesc);
+    close(targetFileDesc);
+
   }
 
   free(m3u);
   free(directory);
-  
-  close(sourceFileDesc);
-  close(targetFileDesc);
-  
+  free(m3uLine);
+    
   fclose(m3uFile);
   
   return 0;
